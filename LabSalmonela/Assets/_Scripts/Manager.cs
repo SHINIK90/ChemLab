d625d154fc2aa67ad00c550592478ca661758d74
SHINIK90 <muzemail8d@gmail.com>
2021-10-04 15:39:36 -0500

    Check mark Whiteboard

--- LabSalmonela/Assets/_Scripts/Manager.cs
+++ LabSalmonela/Assets/_Scripts/Manager.cs
@@ -6,17 +6,7 @@ public class Manager : MonoBehaviour
 {
     public GameObject[] InitialItems;
     public GameObject[] InitialItemsCheck;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     public void InitialItemsGrab(int a){
         InitialItemsCheck[a].SetActive(true);
