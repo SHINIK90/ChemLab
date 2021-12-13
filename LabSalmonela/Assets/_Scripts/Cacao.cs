941c660c28a8376a7be52a784fd10934fb4cdf58
Enemigue <miguelarreguib@hotmail.com>
2021-12-13 14:49:15 -0500

    Outline

---
+++ LabSalmonela/Assets/_Scripts/Cacao.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cacao : MonoBehaviour
+{
+    GameObject granoDeCacap;
+    int cont;
+    void Start()
+    {
+        cont = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
