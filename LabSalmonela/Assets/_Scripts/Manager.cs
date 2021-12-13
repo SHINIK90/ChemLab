8d226c07d301165352278d468de4b820333998f1
Enemigue <miguelarreguib@hotmail.com>
2021-12-12 19:17:37 -0500

    Revert "Detergente"
    This reverts commit d984007da912656623e006704c51ba334250d812, reversing
    changes made to e949928b42032502313e76c3b82737e75d4272da.

--- LabSalmonela/Assets/_Scripts/Manager.cs
+++ LabSalmonela/Assets/_Scripts/Manager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class Manager : MonoBehaviour
 {
@@ -10,28 +9,17 @@ public class Manager : MonoBehaviour
     public GameObject[] InitialItemsCheckTask;
     public GameObject InitialItemsCheckImgs;
     public int InitialItemsCounter = 0;
-    int currentPaso = 1;
-    public GameObject Stains;
-    public GameObject WBStainsText;
     public GameObject UVLight;
-    public GameObject completedText;
     public GameObject TMPaso1;
     public GameObject TMPaso2;
-    public GameObject TMPaso3;
     public GameObject WBPaso1;
     public GameObject WBPaso2;
-    public GameObject WBPaso3;
 
 
     private void Update() {
         if(InitialItemsCounter == 3){
             Invoke("Completed1", 1f);
             Invoke("Start2", 3f);
-        }
-        if(stainCleaned() == 0){
-            currentPaso = 2;
-            Invoke("Completed", 1f);
-            Invoke("Start3", 3f);
         }
     }    
 
@@ -42,35 +30,22 @@ public class Manager : MonoBehaviour
         Destroy(InitialItems[a]);
         InitialItemsCounter++;
     }
-    public int stainCleaned(){
-        int left = Stains.transform.childCount;
-        WBStainsText.GetComponent<TextMeshProUGUI>().text = "Manchas Restantes:\n" + left + "/3";
-        return left;
-    }
     public void Completed1(){
         Destroy(InitialItemsCheckImgs);
         WBPaso1.SetActive(true);
-        TMPaso1.SetActive(false);
+        Destroy(TMPaso1);
         TMPaso2.SetActive(true);
     }
     public void Start2(){
-        WBPaso1.SetActive(false);
+        Destroy(WBPaso1);
         WBPaso2.SetActive(true);
     }
-    public void Start3(){
-        WBPaso1.SetActive(false);
-        WBPaso2.SetActive(false);
-        WBPaso3.SetActive(true);
-    }
+
     public void Bunsen(float a){
         //Debug.Log(a);
     }
 
     public void UV(){
         UVLight.SetActive(!UVLight.activeSelf);
-    }
-    void completed(){
-        completedText.GetComponent<TextMeshProUGUI>().text = "Completado " + currentPaso + "/9";
-        WBPaso1.SetActive(true);
     }
 }
