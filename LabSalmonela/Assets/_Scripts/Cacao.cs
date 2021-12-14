d835a695412415aa17a24c5c0679938acf9ff4b3
Enemigue <miguelarreguib@hotmail.com>
2021-12-13 19:58:30 -0500

    correcciones

--- LabSalmonela/Assets/_Scripts/Cacao.cs
+++ LabSalmonela/Assets/_Scripts/Cacao.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Cacao : MonoBehaviour
 {
-    GameObject granoDeCacap;
+    public GameObject GC;
+    public GameObject advertencia;
+    public GameObject Cacao1;
+    public GameObject Cacao2;
+    public GameObject Cacao3;
     int cont;
     void Start()
     {
@@ -14,6 +18,43 @@ public class Cacao : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (cont == 4)
+        {
+            cont = 3;
+            advertencia.SetActive(true);
+        }
+        if(cont == 1)
+        {
+            Cacao1.SetActive(true);
+        }
+        if (cont == 2)
+        {
+            Cacao2.SetActive(true);
+        }
+        if(cont == 3)
+        {
+            Cacao3.SetActive(true);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Funda")
+        {
+            Debug.Log("SAQUENME DE LATINOAMERICA");
+            GC.SetActive(true);
+        }
+        if (other.tag == "envase")
+        {
+            GC.SetActive(false);
+            cont+=1;
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "envase")
+        {
+            advertencia.SetActive(false);
+        }
     }
 }
