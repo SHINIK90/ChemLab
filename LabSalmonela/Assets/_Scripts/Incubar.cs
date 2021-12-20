5cd1d9d4c157dc576eab2085f3df72b64fd3d43b
Enemigue <miguelarreguib@hotmail.com>
2021-12-20 11:37:20 -0500

    trigger

--- LabSalmonela/Assets/_Scripts/Incubar.cs
+++ LabSalmonela/Assets/_Scripts/Incubar.cs
@@ -6,30 +6,37 @@ public class Incubar : MonoBehaviour
 {
     public Animator m_Animator;
     public bool TieneEnvase = false;
+    public GameObject cerrar;
 
 
     private void OnTriggerStay(Collider other)
     {
+        Debug.Log(other.tag);
         if (other.tag == "envase")
         {
             TieneEnvase = true;
+            cerrar.SetActive(true);
         }
         else
         {
             TieneEnvase = false;
+            cerrar.SetActive(false);
         }
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Entrar" && TieneEnvase == true)
+        {
+            // Debug.Log("Oe serrano bobo");
+            m_Animator.SetTrigger("Active");
+        }
     }
     private void OnTriggerExit(Collider other)
     {  
         if (other.tag == "envase")
         {
             TieneEnvase = false;
-        }
-        if (other.tag == "Entrar" && TieneEnvase == true)
-        {
-           // Debug.Log("Oe serrano bobo");
-            m_Animator.SetTrigger("Active");
         }
       
     }
